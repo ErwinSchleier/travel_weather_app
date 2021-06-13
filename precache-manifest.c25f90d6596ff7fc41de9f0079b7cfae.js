3391e7ef4ed51bb4721883dbd30e807feb1b3376
erwinschleier@googlemail.com <erwin.schleier@stud.uni-bamberg.de>
2021-06-13 14:34:34 +0200

    Updates

---
+++ precache-manifest.c25f90d6596ff7fc41de9f0079b7cfae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ac6d1b9b6e923b0872a2df14ba24a0",
+    "url": "/travel_weather_app/index.html"
+  },
+  {
+    "revision": "07a3537fda5fcba6ead9",
+    "url": "/travel_weather_app/static/css/main.ff01474b.chunk.css"
+  },
+  {
+    "revision": "cf6aa881c6970c16325a",
+    "url": "/travel_weather_app/static/js/2.de69d1a4.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/travel_weather_app/static/js/2.de69d1a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07a3537fda5fcba6ead9",
+    "url": "/travel_weather_app/static/js/main.5f12fad9.chunk.js"
+  },
+  {
+    "revision": "28d0b666987c3058c1a4",
+    "url": "/travel_weather_app/static/js/runtime-main.0501a9e3.js"
+  },
+  {
+    "revision": "d12f0de8cb79f51eee4dc80ec9407f09",
+    "url": "/travel_weather_app/static/media/2850815.d12f0de8.jpg"
+  }
+]);
